3308a92e5cbcc08193e84776530e8b05609de39f
Yura Kaminskyi <yuriy.kaminskyi@gmail.com>
2018-07-06 14:35:45 +0300

    Fix cost

--- code/utils.py
+++ code/utils.py
@@ -1,12 +1,14 @@
 import numpy as np
+
 class Optimizer:
 
-	def __init__(self, lerning_rate):
-		self.lerning_rate = lerning_rate
+    def __init__(self, lerning_rate):
+        self.lerning_rate = lerning_rate
 
 class CategoricalCrossentropy:
-	def __call__(self, prediction, y):
-		m = y.shape[1]
-		cost = (-1 / m) * np.sum(np.multiply(y, np.log(prediction)) +
-                                 np.multiply(1 - y, np.log(1 - prediction)))
-		return cost
+    def __call__(self, prediction, y):
+        m = y.shape[1]
+        logprobs = np.multiply(np.log(prediction), y) +\
+                   np.multiply((1 - y), np.log(1 - prediction))
+        cost = - np.sum(logprobs) / m
+        return cost
