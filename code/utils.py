7478c0123e06b4bc9a59cab06aa26bffa21399d7
Yura Kaminskyi <yuriy.kaminskyi@gmail.com>
2018-07-06 14:32:47 +0300

    Add crossentropy loss

--- code/utils.py
+++ code/utils.py
@@ -1,10 +1,12 @@
-
-
+import numpy as np
 class Optimizer:
 
 	def __init__(self, lerning_rate):
 		self.lerning_rate = lerning_rate
 
 class CategoricalCrossentropy:
-	def __init__(self):
-		pass
+	def __call__(self, prediction, y):
+		m = y.shape[1]
+		cost = (-1 / m) * np.sum(np.multiply(y, np.log(prediction)) +
+                                 np.multiply(1 - y, np.log(1 - prediction)))
+		return cost
